6bf53b0f86323f73bd3f4cac6bd13a8e2d8c8461
Filip3Kx <filip.kubawski@gmail.com>
2023-12-06 12:02:53 +0100

    add asym support

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from google.cloud import kms_v1
+import rsa
 import hashlib
+
 
 def list_keys(project_id, location_id, key_ring_id, crypto_key_id):
     client = kms_v1.KeyManagementServiceClient()
@@ -40,50 +42,53 @@ def symetric_decrypt(project_id, location_id, key_ring_id, crypto_key_id, cipher
     return decrypted_plaintext
 
 
-def asymmetric(project_id, location_id, key_ring_id, crypto_key_id, key_version_id, message):
+
+def verify_key_pair(project_id, location_id, key_ring_id, crypto_key_id, key_version_id, message):
     client = kms_v1.KeyManagementServiceClient()
     key_version_name = client.crypto_key_version_path(project_id, location_id, key_ring_id, crypto_key_id, key_version_id)
     message_bytes = message.encode('utf-8')
-    hash_ = hashlib.sha256(message_bytes).digest()
+    hash_ = hashlib.sha512(message_bytes).digest()
     sign_response = client.asymmetric_sign(
         name=key_version_name,
-        digest={'sha256': hash_}
+        digest={'sha512': hash_}
     )
     signature = sign_response.signature
-    print(signature)
-
-    public_path = client.public_key_path(
-        project = project_id,
-        location = location_id,
-        key_ring=key_ring_id,
-        crypto_key=crypto_key_id,
-        crypto_key_version=key_version_id,
-    )
-    print(public_path)
-
-
+    public_key_response = client.get_public_key(name=key_version_name)
+    public_key = rsa.PublicKey.load_pkcs1_openssl_pem(public_key_response.pem.encode('utf-8'))
+    try:
+        rsa.verify(message_bytes, signature, public_key)
+        print("Key pair is valid.")
+    except rsa.VerificationError:
+        print("Key pair is invalid.")
 
 
 
 # vars symetric
 project_id = "festive-utility-407111"
 location_id = "global"
-key_ring_id = "python"
-crypto_key_id = "python-asymetric-sign"
+key_ring_id = "my-keyring"
+sym_crypto_key_id = "my-symmetric-encryption-key"
+asym_crypto_key_id = "my-asymmetric-signing-key"
 key_version_id = "1"
-
-
 plaintext = "Hello, GCP KMS!"
 
-try:
-    asymmetric(project_id, location_id, key_ring_id, crypto_key_id, key_version_id , plaintext)
-    # list_keys(project_id, location_id, key_ring_id, crypto_key_id)
-
-    # print(symetric_encrypt(project_id, location_id, key_ring_id, crypto_key_id, plaintext))
-
-    # ciphertext = symetric_encrypt(project_id, location_id, key_ring_id, crypto_key_id, plaintext)
-    # print(symetric_decrypt(project_id, location_id, key_ring_id, crypto_key_id, ciphertext))
 
 
-except Exception as e:
-    print(f"Error: {e}")
+choice = input("1. Symetric\n2. Asymetric\nEnter your choice: ")
+
+if choice == "1":
+    try:
+        list_keys(project_id, location_id, key_ring_id, sym_crypto_key_id)
+        print(symetric_encrypt(project_id, location_id, key_ring_id, sym_crypto_key_id, plaintext))
+        ciphertext = symetric_encrypt(project_id, location_id, key_ring_id, sym_crypto_key_id, plaintext)
+        print(symetric_decrypt(project_id, location_id, key_ring_id, sym_crypto_key_id, ciphertext))
+    except Exception as e:
+        print(f"Error: {e}")
+elif choice == "2":
+    try:
+        list_keys(project_id, location_id, key_ring_id, asym_crypto_key_id)
+        verify_key_pair(project_id, location_id, key_ring_id, asym_crypto_key_id, key_version_id, plaintext)
+    except Exception as e:
+        print(f"Error: {e}")
+
+
